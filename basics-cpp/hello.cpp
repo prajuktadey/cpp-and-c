bb513cc01e48cb01a6330d79cf5b1af51346a74c
prajuktadey <theprajuktadey@gmail.com>
2022-07-05 19:05:32 +0530

    Added a file.

---
+++ basics-cpp/hello.cpp
@@ -0,0 +1,9 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" <<std::endl;
+    //std::endl which is used to denote, a new line should be added to the end of our line
+    return 0;
+}
