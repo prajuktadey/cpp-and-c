d14eaa412eb410b0e753e0a1759f52b04eb0a84e
prajuktadey <theprajuktadey@gmail.com>
2022-07-05 19:05:13 +0530

    Deleted files.

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-// Your First C++ Program
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!" <<std::endl;
-    //std::endl which is used to denote, a new line should be added to the end of our line
-    return 0;
-}
