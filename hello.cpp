31f12830f936fb7a507e676b1c2604b058c41354
prajuktadey <theprajuktadey@gmail.com>
2022-07-05 16:45:23 +0530

    Added a file.

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!" <<std::endl;
+    //std::endl which is used to denote, a new line should be added to the end of our line
     return 0;
 }
