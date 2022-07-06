1dc5b2d4753bc4a28466990334cbeebf2e1af6b7
prajuktadey <theprajuktadey@gmail.com>
2022-07-06 20:21:59 +0530

    Added a file.

---
+++ hanoi-tower/uiuc/Cube.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "HSLAPixel.h"
+
+namespace uiuc {
+  class Cube {
+    public:
+      Cube(double length, HSLAPixel color);
+
+      double getLength() const;
+      void setLength(double length);
+
+      double getVolume() const;
+      double getSurfaceArea() const;
+
+    private:
+      double length_;
+      HSLAPixel color_;
+  };
+}
