849671617ea930afdd424db0af250585eb694b93
prajuktadey <theprajuktadey@gmail.com>
2022-07-06 19:43:26 +0530

    Added a file.

---
+++ vectors/vector2.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int>v;
+    for(int i=0;i<100;i++)
+    {
+        v.push_back(i*i);
+    }
+
+    std::cout<<v[12]<<std::endl;
+
+    return 0;
+}
