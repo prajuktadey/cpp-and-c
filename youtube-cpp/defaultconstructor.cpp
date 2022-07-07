79626cdf5078042e12e5f9582909272aa0d217e5
prajuktadey <theprajuktadey@gmail.com>
2022-07-07 16:13:39 +0530

    Added a file.

---
+++ youtube-cpp/defaultconstructor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using std::string;
+
+class Employee
+{
+public: //always change it to public in order to access the class variables
+  string Name;
+  string Company;
+  int Age;
+
+  void IntroduceYourself() //class method
+  {
+    std::cout<<"Name: "<< Name <<std::endl;
+    std::cout<<"Company: "<< Company <<std::endl;
+    std::cout<<"Age: "<< Age <<std::endl;
+  }
+};
+
+int main()
+{
+   Employee employee1; //employee1 is a variable of type Employee
+   //employee1 is an object of class Employee
+
+   /*employee1.Name="Conrad Fisher";//set value for property Name
+   employee1.Company="Netflix";
+   employee1.Age=23;*/
+
+   employee1.IntroduceYourself();
+
+
+   Employee employee2;
+   /*employee2.Name="Isabelle Cooper";//set value for property Name
+   employee2.Company="Netflix";
+   employee2.Age=22;*/
+
+   employee2.IntroduceYourself();
+
+}
+
+/*Name: 
+Company: 
+Age: -1740171520   //this is the work of the default constructor that is already present
+Name: 
+Company: 
+Age: 1356855248*/
