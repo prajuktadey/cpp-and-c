a4a9278b730544c4c6a765dc71a1dabf68b4699b
prajuktadey <theprajuktadey@gmail.com>
2022-07-07 21:31:56 +0530

    Added a file.

---
+++ youtube-cpp/inheritance.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using std::string;
+
+class AbstractEmployee //abstract class
+{
+   virtual void AskForPromotion()=0; //to make the method obligatory
+   //we have to make the function a pure virtual function or an abstract function
+
+};
+
+class Employee: AbstractEmployee //Employee is inheriting from AbstractEmployee
+//whichever class inherits from the abstract employee class, that class will have to provide implementation for the AskForPromotion() method
+{
+private:
+
+  string Name;
+  string Company;
+  int Age;
+  
+
+public: 
+  
+  void setName(string name) 
+  {
+    Name= name;
+  }
+  string getName() 
+  {
+    return Name;  
+  }
+
+  void setCompany(string company) 
+  {
+    Company= company;
+  }
+  string getCompany() 
+  {
+    return Company; 
+  }
+
+  void setAge(int age) 
+  {
+    if(age>=18)
+      Age= age;
+  }
+  int getAge() 
+  {
+    return Age; 
+  }
+
+
+  void IntroduceYourself() 
+  {
+    std::cout<<"Name: "<< Name <<std::endl;
+    std::cout<<"Company: "<< Company <<std::endl;
+    std::cout<<"Age: "<< Age <<std::endl;
+  }
+
+  Employee(string name, string company, int age)
+  {
+     Name= name;
+     Company= company;
+     Age= age;
+  }
+
+  void AskForPromotion() 
+  //since it has inherited from the abstract class, the class Employee has to provide implementation which is written below
+  {
+     if(Age>30)
+       std::cout<<Name<<" got promoted."<<std::endl;
+     else
+       std::cout<<Name<<" sorry, no promotion for you."<<std::endl;
+  }
+};
+
+class Developer: Employee //base class: Employee, derived class: Developer
+{
+   public:
+      string FavProgrammingLanguage;
+      Developer(string name, string company, int age, string favProgrammingLanguage)
+         :Employee(name,company,age)////constructor of base class is going to take care of all the properties except FavProgrammingLanguage
+      {
+        FavProgrammingLanguage= favProgrammingLanguage;
+      }
+
+      void FixBug()
+      {
+        std::cout<<getName()<<" fixed bug using "<<FavProgrammingLanguage<<std::endl;
+        //getName is what we are using to access properties of our Employee class
+        //we are accessing directly properties of the developer class by FavProgrammingLanguage
+      }
+    
+};
+
+int main()
+{
+  
+  Developer d= Developer("Jeremiah", "Netflix", 25, "Python");
+  d.FixBug();
+}
