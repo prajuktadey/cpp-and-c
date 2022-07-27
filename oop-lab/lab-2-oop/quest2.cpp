a283d7c8c4f290f8e59dbfb51e053095b21abd52
prajuktadey <theprajuktadey@gmail.com>
2022-07-27 18:51:40 +0530

    Modified.

--- oop-lab/lab-2-oop/quest2.cpp
+++ oop-lab/lab-2-oop/quest2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     char s1[100], c = 'a';
-    int n = 0, i = 0;
+    int len = 0, i = 0;
 
     cout << "Enter the String you want to reverse: ";
     cin >> s1;
@@ -16,16 +16,16 @@ int main()
         c = s1[i];
         i++;
     }
-    n = i - 1;//length of the string
+    len= i - 1;//length of the string
 
-    cout << "\nLength of the entered string: " << n << "\n";
+    cout << "\nLength of the entered string: " << len << "\n";
 
     char s2[i]; //another char array to store the reverse of a string 
 
     i = 0;
-    while (i != n + 1) //while i is not equal to the last character of the input string 
+    while (i != len + 1) //while i is not equal to the last character of the input string 
     {
-        s2[i] = s1[n - i - 1]; //extracting the characters from the end of the string 
+        s2[i] = s1[len - i - 1]; //extracting the characters from the end of the string 
         i++;
     }
 
