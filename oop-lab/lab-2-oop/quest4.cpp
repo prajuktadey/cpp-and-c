cbc3e38095d674fb493f685ad6ec789f17148821
prajuktadey <theprajuktadey@gmail.com>
2022-07-27 16:59:15 +0530

    Added a file.

---
+++ oop-lab/lab-2-oop/quest4.cpp
@@ -0,0 +1,30 @@
+//wap to compare the two strings using library functions
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string s1;
+    string s2;
+    int a,b;
+
+    cout << "Enter the string 1: ";
+    cin >> s1 ;
+
+    cout << "Enter the string 2: ";
+    cin >> s2 ;
+
+    int cmp= s1.compare(s2);
+    if(cmp==0)
+    {
+        cout<<"The strings are the same.";
+    }
+    else
+    {
+        cout<<"The strings are not the same.";
+    }
+
+    return 0;
+
+}
---
+++ oop-lab/lab-2-oop/quest5.cpp
@@ -0,0 +1,30 @@
+//wap to compare the two strings without using library functions
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str1[50], str2[50];
+    int i=0, cmp=0;
+
+    cout<<"Enter the first string: ";
+    cin>>str1;
+    cout<<"Enter the second string: ";
+    cin>>str2;
+
+    while(str1[i]!='\0' || str2[i]!='\0') //checking for the last character 
+    {
+        if(str1[i]!=str2[i])
+        {
+            cmp = 1;
+            break;
+        }
+        i++;
+    }
+    if(cmp==0)
+        cout<<"\nStrings are equal.";
+    else
+        cout<<"\nStrings are not equal";
+
+    cout<<endl;
+    return 0;
+}
