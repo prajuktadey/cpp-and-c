fbcfef547455ec87d76e50fa7191d74ea03bd4a0
prajuktadey <theprajuktadey@gmail.com>
2022-08-03 00:34:21 +0530

    Added a file.

---
+++ kiit-labs/extra-programs/factorial.cpp
@@ -0,0 +1,26 @@
+/* WAP to calculate the factorial of a given number. */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    long factorial = 1;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Factorial of a negative number doesn't exist.";
+    else 
+    {
+        for(int i = 1; i <= n; i++) 
+        {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
