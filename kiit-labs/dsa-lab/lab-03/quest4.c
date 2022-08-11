a19e7a2fba1abe103697c5b67ac612336abb5683
prajuktadey <theprajuktadey@gmail.com>
2022-08-11 21:25:04 +0530

    Modified.

--- kiit-labs/dsa-lab/lab-03/quest4.c
+++ kiit-labs/dsa-lab/lab-03/quest4.c
@@ -5,28 +5,35 @@ largest number in a dynamic array of n integers using a single loop.*/
 #include <stdlib.h>
 
 int main()
-{
+{   
     int n;
     printf("Enter number of elements: ");
     scanf("%d",&n);
+
     int *a=(int *)malloc(n*sizeof(int));
     printf("Enter the elements: ");
+
     int i;
     for(i=0; i<n; i++)
     scanf("%d",(a+i));
+
     int x=*(a+0);
     for(i=0;i<n;i++)
     printf("%d ",*(a+i));
-    for(i=0; i<n; i++){
+
+    for(i=0; i<n; i++)
+    {
         if (x<*(a+i))
         x=*(a+i);
     }
+    
     int rec=0;
-    for(i=0; i<n; i++){
+    for(i=0; i<n; i++)
+    {
         if(x==*(a+i))
         rec++;
     }
-    printf("\nLargest number = %d\noccurence = %d",x,rec);
 
+    printf("\nLargest number = %d\noccurence = %d",x,rec);
     return 0;
 }
