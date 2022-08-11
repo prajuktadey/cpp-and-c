21a27b41850edcc3293c087ca939cb784551a3a1
prajuktadey <theprajuktadey@gmail.com>
2022-08-11 21:13:14 +0530

    Added files.

--- kiit-labs/dsa-lab/lab-03/quest3.c
+++ kiit-labs/dsa-lab/lab-03/quest3.c
@@ -2,3 +2,52 @@
 corresponding elements in the last column, and 2nd column with the second last
 column and 3rd with 3rd last etc. of a 2-D dynamic array. Display the matrix.*/
 
+#include <stdio.h>
+#include<stdlib.h>
+
+void swap(int *A, int n, int m){
+    int i=0;
+    int j=m-1;
+    int k;
+    while(i<n/2){
+        for(k=0;k<n;k++){
+            int temp = *(A+k*n+i);
+            *(A+k*n+i)=*(A+k*n+j);
+            *(A+k*n+j)=temp;
+        }
+        i++;
+        j--;
+    }
+    printf("\nThe new matrix: \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ",*(A+i*n+j));
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int n,m,i,j;
+    printf("Enter no. of rows: ");
+    scanf("%d",&n);
+    printf("Enter no. of columns: ");
+    scanf("%d",&m);
+    int *A=(int *)malloc(n*m*sizeof(int));
+    printf("Enter elements of matrix: \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",(A+i*n+j));
+        }
+    }
+    printf("\nThe matrix: \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ",*(A+i*n+j));
+        }
+        printf("\n");
+    }
+    swap(A,n,m);
+
+    return 0;
+}
--- kiit-labs/dsa-lab/lab-03/quest4.c
+++ kiit-labs/dsa-lab/lab-03/quest4.c
@@ -1,2 +1,32 @@
 /*Write a program in C to find the largest number and counts the occurrence of the
 largest number in a dynamic array of n integers using a single loop.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int *a=(int *)malloc(n*sizeof(int));
+    printf("Enter the elements: ");
+    int i;
+    for(i=0; i<n; i++)
+    scanf("%d",(a+i));
+    int x=*(a+0);
+    for(i=0;i<n;i++)
+    printf("%d ",*(a+i));
+    for(i=0; i<n; i++){
+        if (x<*(a+i))
+        x=*(a+i);
+    }
+    int rec=0;
+    for(i=0; i<n; i++){
+        if(x==*(a+i))
+        rec++;
+    }
+    printf("\nLargest number = %d\noccurence = %d",x,rec);
+
+    return 0;
+}
