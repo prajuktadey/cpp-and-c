dc78be23e66f22bfa6cee9b61fbd8eaf42cf9fd2
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2022-09-30 11:11:51 +0530

    Added a file.

---
+++ kiit-labs/linked-list/deletefirst.c
@@ -0,0 +1,69 @@
+//deletion of the linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node * next;
+};
+
+void linkedlistTraversal(struct Node * ptr)
+{
+    while(ptr!=NULL)
+    {
+     printf("%d\n", ptr->data);
+     ptr=ptr->next;
+    }
+}
+
+struct Node * DeleteFirst(struct Node *head)
+{
+    struct Node *ptr= head;
+    head=head->next;
+    free(ptr);
+
+    return head;
+}
+
+
+int main()  
+{
+    struct Node * head;
+    struct Node * second;
+    struct Node * third;
+    struct Node * fourth;
+
+    //allocated memory for nodes in the heap
+    head = (struct Node *) malloc (sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *) malloc(sizeof(struct Node));
+
+    //linking of first and second
+    head->data=1;
+    head->next=second;
+
+    //linking of second and third
+    second->data=3;
+    second->next=third;
+
+    //linking third and fourth 
+    third->data=5;
+    third->next=fourth;
+
+    //fourth node
+    fourth->data=7;
+    fourth->next=NULL;
+
+    printf("Linked list before deletion: \n");
+    linkedlistTraversal(head);
+
+    head= DeleteFirst(head);
+    printf("Linked list after deletion: \n");
+    linkedlistTraversal(head);
+
+    return 0;
+}
+
